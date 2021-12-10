7cbb4c0fe2aea3f0dbc221d136bbc06a57782341
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2021-12-10 21:44:06 +0100

    feat: update styles for visited tiles

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -9,6 +9,29 @@ type TileProps = {
   onClick: () => void;
 } & Mine;
 
+const getTileColor = (neighborCount?: number): string => {
+  switch (neighborCount) {
+    case 1:
+      return Colors.BLUE3;
+    case 2:
+      return Colors.GREEN3;
+    case 3:
+      return Colors.ORANGE3;
+    case 4:
+      return Colors.RED3;
+    case 5:
+      return Colors.TURQUOISE3;
+    case 6:
+      return Colors.LIME3;
+    case 7:
+      return Colors.GOLD3;
+    case 8:
+      return Colors.SEPIA3;
+    default:
+      return '';
+  }
+};
+
 const Tile = ({
   isVisited,
   isMine,
@@ -16,21 +39,29 @@ const Tile = ({
   onClick,
 }: TileProps): JSX.Element => {
   if (!isVisited) {
-    return <StyledTileWrapper onClick={onClick} />
+    return <StyledTileWrapper onClick={onClick} />;
   }
 
   if (!isMine) {
-    return <StyledTileWrapper>{neighborCount}</StyledTileWrapper>
+    return (
+      <StyledTileWrapper $color={getTileColor(neighborCount)} $isVisited={isVisited}>
+        {neighborCount || ""}
+      </StyledTileWrapper>
+    );
   }
-  
-  return <StyledTileWrapper>X</StyledTileWrapper>;
+
+  return <StyledTileWrapper $isVisited={isVisited}>X</StyledTileWrapper>;
 };
 
-const StyledTileWrapper = styled.button`
+const StyledTileWrapper = styled.button<{ $color?: string, $isVisited?: boolean; }>`
   align-items: center;
-  background-color: ${Colors.LIGHT_GRAY5};
-  border: 1px solid ${Colors.LIGHT_GRAY3};
+  background-color: ${({ $isVisited }) => $isVisited ? Colors.LIGHT_GRAY5 : Colors.LIGHT_GRAY3};
+  border: 1px solid ${Colors.LIGHT_GRAY1};
+  color: ${({ $color }) => $color || Colors.DARK_GRAY1};
   display: flex;
+  font-family: "Space Mono", monospace;
+  font-size: 16px;
+  font-weight: bold;
   height: ${TILE_SIZE}px;
   justify-content: center;
   outline: none;
